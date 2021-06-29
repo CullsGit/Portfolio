a6f0dfcfe17a89c216d2fa7c841d63042ff8e32e
Hugo <hugo.cullinan@gmail.com>
2021-06-30 09:45:15 +1000

    cookbook-project

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,21 +11,30 @@ export const headData = {
 export const heroData = {
   title: 'Hello, my name is',
   name: 'Hugo Cullinan',
-  subtitle: 'I\'m a keen Software Engineer',
+  subtitle: `'I'm a keen Software Engineer'`,
   cta: 'About me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'bwheadshot.jpg',
-  paragraphOne: 'I have recently discontinued in the hospitality industry as a chef, and have decided to exercise my mind in the field of software engineering. It became evident that my desire to learn new things, and my constant curiosity of how and why things work was a big factor in working my way up through the ranks at my previous job.',
-  paragraphTwo: 'I chose software engineering as I find the problem solving aspect extremely gratifying, and I believe the soft skills I have acquired will be pertinent in such a collaborative field. I am extremely excited to begin my career in the industry and get stuck into some inspiring ideas.',
-  paragraphThree: '',
+  paragraphOne: `Whether it be the logistics of a single trip grocery carry or nailing a tricky parallel park, my entire day is rife with problem-solving.`,
+  paragraphTwo: `After several years as a chef in a busy cafe, I learned to appreciate how rewarding solving problems can be. I sparked an interest in coding while still working full time. The more I exercised my brain coding, the clearer my passion for the craft became. Much like following a recipe, coding requires actions to happen in a certain way, often following a specific order. It was time to sharpen my blade in a new discipline.`,
+  paragraphThree: 'I am motivated by innovative ideas with high impact. I desire to work within an encouraging team that values collaboration. Always keen to build, break, test, solve and learn new things.',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'myrecipes.png',
+    title: 'Cookbook App',
+    info: 'Front-end application, built with react',
+    info2: 'Utilises react-bootstrap, react-hook-form, hooks, uuid',
+    url: 'https://cookbook-36682.web.app/',
+    repo: 'https://github.com/CullsGit/cookbook', // if no repo, the button will not show up
+  },
   {
     id: nanoid(),
     img: 'funfacts.png',
